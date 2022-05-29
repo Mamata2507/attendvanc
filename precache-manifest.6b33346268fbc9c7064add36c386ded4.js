0964554c3b3bf6f58c48baed400ab41b36611d0f
Kunal Yadav <kunal99356@gmail.com>
2022-05-29 17:46:50 +0530

    Updates

---
+++ precache-manifest.6b33346268fbc9c7064add36c386ded4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e9e3544ba9b0e009afbdbf20f6e1cca",
+    "url": "/attendvanc/index.html"
+  },
+  {
+    "revision": "301153268cda8d8ae683",
+    "url": "/attendvanc/static/css/2.34fcde48.chunk.css"
+  },
+  {
+    "revision": "765ab6146c127e413723",
+    "url": "/attendvanc/static/css/main.5dd63815.chunk.css"
+  },
+  {
+    "revision": "301153268cda8d8ae683",
+    "url": "/attendvanc/static/js/2.374a09a8.chunk.js"
+  },
+  {
+    "revision": "b97446142338735ef518a8a1e32390ee",
+    "url": "/attendvanc/static/js/2.374a09a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "765ab6146c127e413723",
+    "url": "/attendvanc/static/js/main.897e2a20.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/attendvanc/static/js/main.897e2a20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c915c3c5fcdc910dc468",
+    "url": "/attendvanc/static/js/runtime-main.2a3a5261.js"
+  }
+]);
